8349a0e7ddef48f46385db54840a9be57f3ef81e
bianca <bianca@nic.br>
2018-02-15 16:33:18 -0200

    Colocando um formulário

--- alurapic/client/app/cadastro/cadastro.component.ts
+++ alurapic/client/app/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FotoComponent } from '../foto/foto.component';
 
 @Component({
 	moduleId: module.id,
@@ -6,4 +7,19 @@ import { Component } from '@angular/core';
 	templateUrl: './cadastro.component.html'
 })
 
-export class CadastroComponent{}
+export class CadastroComponent{
+	
+	foto: FotoComponent = new FotoComponent();
+
+	constructor(){
+		this.foto.titulo = 'A';
+		this.foto.url = 'B';
+		this.foto.descricao = 'C';
+	}
+
+	cadastrar(event){
+		event.preventDefault();
+		console.log(this.foto);
+	}
+
+}
